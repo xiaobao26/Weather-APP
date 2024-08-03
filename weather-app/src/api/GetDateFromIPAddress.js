94e3e2794264267a7dd577c6ca7319ccbfd636c0
xiaobao26 <xiaobao.xue@outlook.com>
2024-08-03 23:17:13 +1000

    test

--- weather-app/src/api/GetDateFromIPAddress.js
+++ weather-app/src/api/GetDateFromIPAddress.js
@@ -7,7 +7,10 @@ export const getDateFromIPAddress = async () => {
         const response = await axios.get(`https://api.weatherapi.com/v1/ip.json?q=2001%3A8003%3A265a%3Ad800%3A6dd6%3A7bd7%3A49a8%3A56e3%25&key=${apiKey}`);
         return response.data;
     } catch (error) {
-        console.log('get current user IP address error:', error);
+        console.error('Error message:', error.message);
+        console.error('Error code:', error.code);
+        console.error('Error response data:', error.response ? error.response.data : 'No response data');
+        console.error('Error config:', error.config);
     }
     
 }
