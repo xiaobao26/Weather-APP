880e6f3b8827be6ae4c74995565410cfd73639ed
xiaobao26 <xiaobao.xue@outlook.com>
2024-08-03 12:25:38 +1000

    refactor: improve readility and maintainability of environment details part of current city

--- weather-app/src/components/components/components/CurCity/CurCity.jsx
+++ weather-app/src/components/components/components/CurCity/CurCity.jsx
@@ -11,40 +11,57 @@ import pmIcon from '../../../../assets/meta_icon/PM2.5.svg';
 import somatosensoryTemperatureIcon from '../../../../assets/meta_icon/Somatosensory_temperature.svg';
 import speedIcon from '../../../../assets/meta_icon/wind_speed.svg';
 
-export const CurCity = ({ cityname, date, temperature, humanity, speed, pm2_5, flee_like, weathericon}) => {
+export const CurCity = ({ cityname, date, temperature, humanity, speed, pm2_5, flee_like, weathericon }) => {
+    
+    const environmentIcons = [humidityIcon, pmIcon, somatosensoryTemperatureIcon, speedIcon];
+    const environmentData = [
+        {value: `${humanity}%`, label: 'humanity'},
+        {value: `${speed}km/h`, label:'speed'},
+        {value: `${pm2_5}µg`, label:'pm2_5'},
+        {value: `${flee_like}°`, label:'flee_like'},
+
+    ]
 
     return (
-        <div className="col-span-2-3 row-span-2-5 relative flex flex-col rounded-3xl bg-gradient-to-r from-cur-sky to-cur-floor ">
+        <div className="w-16 lg:w-64 md:w-52 col-span-2-3 row-span-2-5 relative flex flex-col rounded-3xl bg-gradient-to-r from-cur-sky to-cur-floor ">
             <img className='absolute' src={curBg} alt="background image absolute" />
-            <ThirdText text={date} color={'#fff'}/>
-            
+            <ThirdText text={date} color={'#fff'} />
+
             <div className='relative w-full h-full flex flex-col justify-around items-center z-10 text-white'>
-                <PrimaryText text={cityname} color={'#ffff'}/>
+                <PrimaryText text={cityname} color={'#ffff'} />
                 <div className='flex flex-col w-full items-center justify-center'>
-                    <Temperature temperature={temperature}/>
+                    <Temperature temperature={temperature} />
                     <TemperatureRange range={`${temperature} ~ ${temperature + 3}°`} />
                 </div>
                 <WeatherIcon iconURL={weathericon} />
 
-                <div className="infos flex items-center justify-around w-7/8 h-1/6 rounded-3xl" style={{backgroundColor: '#F2F3FD'}}>
-                    <div className="info">
-                        <img src={humidityIcon} alt="humanity icon" />
-                        <div className='text-black'>{humanity}%</div>
-                    </div>
-                    <div className="info">
-                        <img src={speedIcon} alt="humanity icon" />
-                        <div className='text-black'>{speed}km/h</div>
-                    </div>
-                    <div className="info">
-                        <img src={pmIcon} alt="humanity icon" />
-                        <div className='text-black'>{pm2_5}&mu;g</div>
-                    </div>
-                    <div className="info">
-                        <img src={somatosensoryTemperatureIcon} alt="humanity icon" />
-                        <div className='text-black'>{flee_like}°</div>
-                    </div>
+                <div className="infos flex items-center justify-around w-7/8 h-1/6 rounded-3xl" style={{ backgroundColor: '#F2F3FD' }}>
+                    {environmentIcons.map((icon, index) => (
+                        <div className="info">
+                            <img className='lg:w-8 md:w-6 sm:w-4' src={icon} alt={`${environmentData[index].label} icon`} />
+                            <div className='text-black text-xs sm:text-sm md:text-xs lg:text-sm xl:text-xl'>{environmentData[index].value}</div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
     );
 };
+
+
+{/* <div className="info">
+        <img src={humidityIcon} alt="humanity icon" />
+        <div className='text-black text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{humanity}%</div>
+    </div>
+    <div className="info">
+        <img src={speedIcon} alt="humanity icon" />
+        <div className='text-black'>{speed}km/h</div>
+    </div>
+    <div className="info">
+        <img src={pmIcon} alt="humanity icon" />
+        <div className='text-black'>{pm2_5}&mu;g</div>
+    </div>
+    <div className="info">
+        <img src={somatosensoryTemperatureIcon} alt="humanity icon" />
+        <div className='text-black'>{flee_like}°</div>
+    </div> */}
